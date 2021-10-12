aa6f92f315cd9354b0487a4f7fb516a5f9505d26
Minu Kim <minu.kim2003@gmail.com>
2021-10-12 23:47:40 +0200

    api fetch success

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
 import { NavBar } from "./components/NavBar";
 import { PokemonCard } from "./components/PokemonCard";
+import { useState, useEffect } from "react";
 
 function App() {
-  const pokemons = [];
+  const [pokemons, setPokemons] = useState(null);
 
-  const fetchData = async () => {
-    const res = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=10");
-    pokemons.push(res);
-  };
-  fetchData();
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=10");
+      const data = await res.json();
+      const pokemons1 = data.results;
+      const pokemons2 = pokemons1.map((element) => {});
+
+      setPokemons(data.results);
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
       <NavBar></NavBar>
       <div className="row">
-        {pokemons.map((element) => {
-          return (
-            <div
-              className="col-6 col-sm-4 col-md-3 col-lg-2 d-flex justify-content-center"
-              key={`pokemon-${element.id}`}
-            >
-              <PokemonCard
-                prefix={element.id}
-                name={element.name}
-                imgSrc={element.image}
-              />
-            </div>
-          );
-        })}
+        {pokemons != null &&
+          pokemons.map((element) => {
+            return (
+              <div
+                className="col-6 col-sm-4 col-md-3 col-lg-2 d-flex justify-content-center"
+                key={`pokemon`}
+              >
+                <PokemonCard name={element.name} />
+              </div>
+            );
+          })}
       </div>
     </div>
   );
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,18 +1,11 @@
-export const PokemonCard = ({ prefix, name, imgSrc }) => {
+export const PokemonCard = ({ name }) => {
   return (
     <div
       className="border d-flex justify-content-center bg-light"
       style={{ width: "150px", height: "150px" }}
     >
       <figure className="figure">
-        <img
-          src={imgSrc}
-          className="figure-img img-fluid rounded "
-          alt="Pikachu"
-        />
-        <figcaption className="figure-caption text-center">
-          #{prefix} {name}
-        </figcaption>
+        <figcaption className="figure-caption text-center">{name}</figcaption>
       </figure>
     </div>
   );
